75cc63b3cd6d279b05a695fadbc995ac09b10dec
Amal Hassen <amalhassen991@gmail.com>
2026-05-26 21:03:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const departments = document.querySelectorAll(".department");
 
+departments.forEach((department) => {
+    department.addEventListener("click", () => {
+        alert(`Navigating to ${department.textContent}`);
+    });
+});
